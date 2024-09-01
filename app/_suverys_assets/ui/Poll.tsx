935d2ab13038ac72fe35dae90f59c4189fc656ec
Iván Uría <ivan.uria@gmail.com>
2024-09-01 23:08:19 +0200

    Añadir Poll

---
+++ app/_suverys_assets/ui/Poll.tsx
@@ -0,0 +1,16 @@
+import { Question } from './Quedtion.tsx';
+
+const pollsTest = [
+
+];
+
+function renderPolls(polls) {
+
+}
+
+export default function Poll() {
+  const polls = [...pollsTest]
+  return (
+
+  );
+}
